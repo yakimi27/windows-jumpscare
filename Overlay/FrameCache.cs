31ac4f93a1c43be2ecaa71860254297e0b9f8d8a
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-04-09 19:22:55 +0300

    add frame caching for better performance

---
+++ Overlay/FrameCache.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace Overlay
+{
+    internal class FrameCache
+    {
+        private readonly List<BitmapImage> _frame = new List<BitmapImage>();
+        public IReadOnlyList<BitmapImage> Frames => _frame;
+
+        public void Preload(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri($"pack://application:,,,/Assets/{i}.png");
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.CreateOptions = BitmapCreateOptions.None;
+                bitmap.EndInit();
+                bitmap.Freeze();
+                _frame.Add(bitmap);
+            }
+        }
+    }
+}
