da7acb18b0a233217702d96d3b0489c3a6ac82c0
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-04-27 10:05:59 +0300

    add models

---
+++ Core/ConfigModels/JumpscareModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.ConfigModels
+{
+    internal class JumpscareModel
+    {
+        public class JumpscareList
+        {
+            public List<Jumpscare> Jumpscares { get; set; }
+        }
+        public class Jumpscare
+        {
+            public string Name { get; set; }
+            public string AssetPath { get; set; }
+            public string FrameAmount { get; set; }
+            public byte FrameFrequency { get; set; }
+        }
+    }
+}
---
+++ Core/ConfigModels/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.ConfigModels
+{
+    internal class UserModel
+    {
+        public ushort JumpscareChance { get; set; } = 10000; //max65536
+
+    }
+}
