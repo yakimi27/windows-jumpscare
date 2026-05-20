eff3b5d20ee6058f4bbf902957a5159cf39d25c3
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-05-21 01:23:50 +0300

    feat: create interfaces for jumpscare and user managers

---
+++ Core/Interfaces/IJumpscareManager.cs
@@ -0,0 +1,13 @@
+﻿using Core.ConfigModels;
+
+namespace Core.Interfaces
+{
+    public interface IJumpscareManager
+    {
+        IReadOnlyList<JumpscareModel.Jumpscare> GetAll();
+        JumpscareModel.Jumpscare GetByName(string name);
+        void Add(JumpscareModel.Jumpscare jumpscare);
+        void Remove(string name);
+        void Update(JumpscareModel.Jumpscare updated);
+    }
+}
---
+++ Core/Interfaces/IUserManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Interfaces
+{
+    public interface IUserManager
+    {
+        ushort GetJumpscareChance();
+        string GetSelectedJumpscare();
+        void SetJumpscareChance(ushort chance);
+        void SetSelectedJumpscare(string jumpscare);
+        void Reset();
+    }
+}
