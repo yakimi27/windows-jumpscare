45f6742dc58839ba81d65d7e33c0652eb85327c8
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-09-03 17:52:40 +0300

    refactor(desktop): move JumpscareWindow to Desktop.Views namespace

--- Desktop/Views/JumpscareWindow.xaml.cs
+++ Desktop/Views/JumpscareWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
-namespace Desktop
+namespace Desktop.Views
 {
     public partial class JumpscareWindow : Window
     {
