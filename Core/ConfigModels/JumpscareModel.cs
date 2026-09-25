ae7c7b04a3b1483889e70329023d868a540cb115
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-09-25 16:49:49 +0300

    fix(jumpscareModel): initialize properties with default values

--- Core/ConfigModels/JumpscareModel.cs
+++ Core/ConfigModels/JumpscareModel.cs
@@ -1,15 +1,15 @@
-﻿namespace Core.ConfigModels
+namespace Core.ConfigModels
 {
     public class JumpscareModel
     {
         public class JumpscareList
         {
-            public List<Jumpscare> Jumpscares { get; set; }
+            public List<Jumpscare> Jumpscares { get; set; } = [];
         }
         public class Jumpscare
         {
-            public string Name { get; set; }
-            public string AssetsPath { get; set; }
+            public string Name { get; set; } = string.Empty;
+            public string AssetsPath { get; set; } = string.Empty;
             public byte FrameAmount { get; set; }
             public byte FrameFrequency { get; set; }
         }
