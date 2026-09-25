0f6b921b176ac3d7e6cd45e6329d20bdd31e5e83
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-09-25 16:10:52 +0300

    refactor(Loop): enhance JumpscareChance property and update possibility logic

--- Core/Loop.cs
+++ Core/Loop.cs
@@ -1,19 +1,33 @@
-﻿namespace Core
+namespace Core
 {
     public class Loop
     {
         private readonly Random _random = new Random();
         private bool _running;
+        private volatile int _posibility;
 
         public event Action? OnTriggered;
 
+        public int JumpscareChance
+        {
+            get => _posibility;
+            set => UpdatePossibility(value);
+        }
+
+        public void UpdatePossibility(int newPossibility)
+        {
+            _posibility = newPossibility;
+        }
+
         public async Task StartAsync(int posibility)
         {
+            _posibility = posibility;
             _running = true;
 
             while (_running)
             {
-                if (_random.Next(posibility) == 0)
+                int currentPossibility = _posibility;
+                if (currentPossibility > 0 && _random.Next(currentPossibility) == 0)
                 {
                     OnTriggered?.Invoke();
                 }
