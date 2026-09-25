8d7a50c1be3813f20a651ac56d2294ca0695d8a6
yakimi27 <139495869+yakimi27@users.noreply.github.com>
2026-09-25 16:50:03 +0300

    fix(iJumpscareManager): update method signatures to allow nullable returns

--- Core/Interfaces/IJumpscareManager.cs
+++ Core/Interfaces/IJumpscareManager.cs
@@ -1,11 +1,13 @@
-﻿using Core.ConfigModels;
+using Core.ConfigModels;
 
 namespace Core.Interfaces
 {
     public interface IJumpscareManager
     {
         IReadOnlyList<JumpscareModel.Jumpscare> GetAll();
-        JumpscareModel.Jumpscare GetByName(string name);
+        JumpscareModel.Jumpscare? GetByName(string name);
+        JumpscareModel.Jumpscare? GetFirstValidJumpscare();
+        string? GetFirstValidJumpscareName();
         void Add(JumpscareModel.Jumpscare jumpscare);
         void Remove(string name);
         void Update(JumpscareModel.Jumpscare updated);
